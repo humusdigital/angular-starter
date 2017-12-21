efe1e0e92e472e110e45eece47994b5d1bc20b89
DamianColazo <damiancolazo@gmail.com>
2017-12-21 18:29:34 -0300

    Commit 1

--- documents/documentController.js
+++
@@ -1,4 +0,0 @@
-myApp.controller('documentctrl', ['$scope', '$log','$rootScope', function($scope, $log, $rootScope) {
-
-    $log.log('is Working document!')
-}]);
--- services/apiservices.js
+++ services/apiservices.js
@@ -1,16 +1,43 @@
-angular.module('starter.services', [])
+angular.module('starterApp.services', [])
 
 .service('api', ['$http', function($http){
 
-  var var;
+    console.log ('Sevices ON');
+    var apiUrl = 'http://www.goblackdiamond.com/wp-json/jwt-auth/v1/token/';
+    var apiUrlPosts ='http://www.goblackdiamond.com/wp-json/wp/v2/';
 
     return {
        events: [],
 
-       mycalll: function(){
+       getTocken: function(){
+
+         var datParams;
+
+         datParams = {
+           username: "apApi",
+           password: "CQ(JXH9RdpqBhhB(0i%$0&MZ",
+
+         }
+         return $http({
+           method: 'post',
+           url: apiUrl,
+           timeout: 10000,
+           params: datParams,
+         });
 
        },
+       getContent: function(){
 
-}
+         comsole.log (token);
 
+         var myQuery ='configuracion/42';
+         return $http({
+           method: 'GET',
+           url: apiUrlPosts+myQuery,
+           timeout: 10000,
+           headers: {
+             'Authorization':'Bearer'+token
+           }
+         });
+}}
 }]);
